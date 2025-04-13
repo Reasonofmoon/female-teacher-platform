52e920c8a78ce13dfb2d5868781220d9564f1066
Reasonofmoon <soundfury37@gmail.com>
2025-04-13 16:46:34 +0900

    React 프로젝트 구조 생성 및 초기 파일 추가

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>여성 교사 플랫폼에 오신 것을 환영합니다</h1>
+        <p>사이트가 현재 구축 중입니다.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
